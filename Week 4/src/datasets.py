1e2a419a55290d96843e63d9cddc1895a40308e3
souparnimazumder <118285021+souparnimazumder@users.noreply.github.com>
2026-01-18 20:07:19 +0100

    Starting final project

---
+++ Week 4/src/datasets.py
@@ -0,0 +1,35 @@
+import os
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_dataloaders(data_dir, img_size=128, batch_size=32, augment=False):
+    train_tfms = [
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor()
+    ]
+    if augment:
+        train_tfms = [
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            transforms.Resize((img_size, img_size)),
+            transforms.ToTensor()
+        ]
+
+    val_tfms = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor()
+    ])
+
+    train_ds = datasets.ImageFolder(os.path.join(data_dir, 'train'),
+                                    transform=transforms.Compose(train_tfms))
+    val_ds = datasets.ImageFolder(os.path.join(data_dir, 'val'),
+                                  transform=val_tfms)
+    test_ds = datasets.ImageFolder(os.path.join(data_dir, 'test'),
+                                   transform=val_tfms)
+
+    return (
+        DataLoader(train_ds, batch_size=batch_size, shuffle=True),
+        DataLoader(val_ds, batch_size=batch_size),
+        DataLoader(test_ds, batch_size=batch_size),
+        train_ds.classes
+    )
